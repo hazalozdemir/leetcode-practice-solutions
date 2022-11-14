055ed8fa4cfc3d5141885fed2cc039a2e87dc266
hazalozdemir <31111722+hazalozdemir@users.noreply.github.com>
2022-11-14 22:59:23 +0300

    TopKFreqeuntElements questions is solved

---
+++ src/TopKFrequentElements.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class TopKFrequentElements {
+    public int[] topKFrequent(int[] nums, int k) {
+
+        Map<Integer, Integer> counterMap = new HashMap<>();
+
+        for (int num : nums) {
+            int counter = counterMap.getOrDefault(num, 0);
+            counterMap.put(num, counter + 1);
+        }
+
+        return counterMap.entrySet().stream().sorted(Map.Entry.<Integer, Integer>comparingByValue()
+                .reversed()).limit(k).mapToInt(integerIntegerEntry -> integerIntegerEntry.getKey()).toArray();
+    }
+}
